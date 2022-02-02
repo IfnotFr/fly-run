dbc56e611a9a9d40828be97fa5444b22a52f39b3
Anael FAVRE <anael.favre@gmail.com>
2022-02-02 10:02:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Fly Run
+
+Simple multiple process spawner and manager for developers.
+
+## Quick start
+
+**1. Install** the package globally with `npm global install fly-run` or `yarn add -g fly-sync`
+
+**2. Configure** it with a js/json file (example `/home/me/fly-run.conf.js`) :
+
+```js
+module.exports = [
+  {
+    name: 'prog1',
+    command: '/usr/bin/yarn',
+    args: ['serve'],
+    options: {
+      cwd: '/home/me/projects/prog1',
+    }
+  },
+  // Add more programs here
+]
+```
+
+**3. Run** with configuration file as second argument `fly-sync /home/me/fly-run.conf.js`
+
+## Commands
+
+Fly run will listen inputs for running some actions :
+
+- **CTRL + C**: Kill all spawned processes and exit the program
+- `kill prog1`: Kill the process prog1
+- `start prog1`: Start the process prog1
+- `restart prog1`: Restart the process prog1
+
+## Configuration (for each entries)
+
+- **name** `string`: The name of the program (used for display and control commands)
+- **command** `string`: The command to run
+- (optionnal) **args** `array[string]`: The array of arguments used for the [spawn](https://nodejs.org/api/child_process.html#child_processspawncommand-args-options) command
+- (optionnal) **options** `array[mixed]`: The array of options used for the [spawn](https://nodejs.org/api/child_process.html#child_processspawncommand-args-options) command
+- (optionnal) **every** `number`: Interval in seconds for running again this program
+  - Only for self-closing programs, handy for running crons (eg. each minute with `every: 60`).
+  
+## Todo List :
+
+- [ ] Major refactoring for splitting main features into modules
+- [ ] Check for process before running it again with `start` command
---
+++ index.js
@@ -0,0 +1,101 @@
+const { spawn } = require('child_process')
+const chalk = require('chalk')
+const onExit = require('death')
+const kill = require('tree-kill')
+const IOSplit = require('iosplit')
+const apps = require('./' + process.argv[2])
+
+/*
+ * Handle input commands and split the screen
+ */
+const iosplit = new IOSplit()
+iosplit.on('line', line => {
+  try {
+    if (line.startsWith('kill ')) {
+      const name = line.replace('kill ', '')
+      console.log(chalk.yellow('Sending kill to', name, ' ...'))
+      kill(childs[name].pid)
+    } else if (line.startsWith('start ')) {
+      const name = line.replace('start ', '')
+      run(apps.find(app => app.name === name))
+    } else if (line.startsWith('restart ')) {
+      const name = line.replace('restart ', '')
+      console.log(chalk.yellow('Sending kill to', name, ' ...'))
+      kill(childs[name].pid)
+      const interval = setInterval(() => {
+        if (!(name in childs)) {
+          run(apps.find(app => app.name === name))
+          clearInterval(interval)
+        }
+      }, 100)
+    }
+  } catch (err) {
+    console.error(err)
+  }
+})
+iosplit.start()
+
+/*
+ * Run the apps
+ */
+const childs = {}
+const run = app => {
+  const count = 'count' in app ? app.count : 1
+  const output = 'output' in app ? app.output : true
+  for (let i = 0; i < count; i++) {
+    childs[app.name] = app.child = spawn(app.command, app.args, app.options)
+    console.log(
+      chalk.green(app.name) +
+      ' - Spawning "' + app.command + ' ' +
+      app.args.join(' ') + '" ' +
+      (!output ? 'IN SILENT MODE ' : ' ') +
+      '...'
+    )
+
+    if (output) {
+      childs[app.name].stdout.on('data', data => {
+        data.toString().split(/\r?\n/).forEach(line => {
+          line = line.replace(/\s+$/, '')
+          if (line !== '') console.log(chalk.blue(app.name) + ' - ' + line)
+        })
+      })
+      childs[app.name].stderr.on('data', data => {
+        data.toString().split(/\r?\n/).forEach(line => {
+          line = line.replace(/~+$/, '')
+          if (line !== '') console.log(chalk.red(app.name) + ' - ' + line)
+        })
+      })
+    }
+    childs[app.name].on('close', code => {
+      console.log(chalk.red(app.name) + ' - CLOSED with code ' + code)
+      delete childs[app.name]
+    })
+  }
+}
+
+apps.forEach(app => {
+  if ('every' in app) {
+    setInterval(() => {
+      run(app)
+    }, app.every * 1000)
+  }
+
+  run(app)
+})
+
+/*
+ * Register exit process cleanups
+ */
+onExit(() => {
+  setInterval(() => {
+    const childCount = Object.entries(childs).length
+    if (childCount === 0) {
+      process.exit()
+    }
+  }, 100)
+
+  for (const [name, child] of Object.entries(childs)) {
+    console.log(chalk.yellow('Sending kill to', name, ' ...'))
+    kill(child.pid)
+  }
+})
